6c59698641fa493c1b15de45b4a280a5058a201d
Jasmyn Newton <jasmyn.n.newton@gmail.com>
2017-03-05 10:48:14 -0500

    Started .js file

---
+++ javascript.js
@@ -0,0 +1,14 @@
+// Array of Questions and Answers
+var questions = [
+              ["What is your mother's maiden name?", "Newton"],
+              ["What was your first pet's name?", "Paris"],
+              ["What do you use to brush your teeth?", "Toothbrush"],
+              ["What street did you grow up on?", "Hampton"],
+              ["Who was your childhood hero?", "Xena"],
+              ["In which drawer do you keep your dish towels?", "Right"]
+            ];
+
+var sel = document.getElementById('QuestionsSelection');
+for(var i = 0; i < questions.length; i++) {
+  var question =
+}
