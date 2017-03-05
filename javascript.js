099e834004d1bdfe87a720dd46f86b368f75fcc6
Travis Kelly <Travis@travis-2.local>
2017-03-05 12:24:35 -0500

    Working array, everything is in index.html, fuck javascript.js

--- javascript.js
+++ javascript.js
@@ -1,22 +1,22 @@
 // Array of Questions and Answers
 var questionsArray = [
-              ["What is your mother's maiden name?", "Lake"],
-              ["What was your first pet's name?", "Paris"],
-              ["What do you use to brush your teeth?", "Toothbrush"],
-              ["What street did you grow up on?", "Hampton"],
-              ["Who was your childhood hero?", "Xena"],
-              ["In which drawer do you keep your dish towels?", "Top"],
-              ["In what year was your first child born?", "1991"]
+              "What is your mother's maiden name?",
+              "What was your first pet's name?",
+              "What do you use to brush your teeth?",
+              "What street did you grow up on?",
+              "Who was your childhood hero?",
+              "In which drawer do you keep your dish towels?",
+              "In what year was your first child born?"
             ];
 
-function getQuestions() {
+/*function getQuestions() {
   var arrayLength = questionsArray.length;
   var questionSet = [];
-  var rand = questionsArray[Math.floor(Math.random() * arrayLength);
+  var rand = questionsArray[Math.floor(Math.random() * arrayLength)];
 
   for(var i = 0; i < 4; i++) {
-    questionSet.push("<h1>" + questionsArray[rand][0] + "</h1><br>");
+    questionSet.push("<h1>" + questionsArray[rand] + "</h1><br>")
   }
 
-  document.getElementById('questionOutput').innerHTML += questionSet.toString();
-}
+  return questionSet.toString();
+}; */
